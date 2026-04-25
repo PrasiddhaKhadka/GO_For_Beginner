26341b440f278a53bf563b4b68f1d7b70fa48378
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-04-25 21:14:38 +0545

    Add:Waitgroup

---
+++ routines/routines.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(id int) {
+	fmt.Println("Doing task", id)
+}
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		go task(i)
+	}
+
+	time.Sleep(time.Second * 2)
+
+}
---
+++ waitgroup/waitgroup.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+//  wait groups:
+
+func task(id int, w *sync.WaitGroup) {
+	// defer only run after the completion of func
+	defer w.Done()
+	fmt.Println("Doing task: ", id)
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		// changing the value from 1 to 0 -> of same memory reference
+		go task(i, &wg)
+	}
+
+	wg.Wait()
+}

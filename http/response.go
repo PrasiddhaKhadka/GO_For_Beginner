bbf92586a280d0abd2c71aabbfa2807e393bb028
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-04-27 06:22:12 +0545

    Add: http response

--- http/response.go
+++ http/response.go
@@ -32,11 +32,36 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, user)
 }
 
+// creating user
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{
+			"error": "method not allowed",
+		})
+		return
+	}
+
+	var req User
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeJSON(w, http.StatusInternalServerError, 0)
+		return
+	}
+
+	if req.Name == "admin" {
+		writeJSON(w, http.StatusOK, map[string]string{"message": "Welcome admin!"})
+	} else {
+		writeJSON(w, http.StatusOK, map[string]string{"message": "Invalid credentials"})
+	}
+
+}
+
 func main() {
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/user", getUser)
+	mux.HandleFunc("/login", login)
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		http.Error(nil, "Server failed", http.StatusInternalServerError)
